31be8573c6091240b3365aada7397e9bb3a10eda
shuangsong <shuangsong6@gmail.com>
2015-09-04 03:05:20 -0400

    :pig_nose:

--- problemset2_2.py
+++ problemset2_2.py
@@ -9,3 +9,16 @@ while balance > 0:
          balance = balance - mini + (balance - mini)*rate
        
 print  'Lowest Payment: '  + str(mini)
+
+#below is that I wrote to submit :
+#balance and annualinterestrate is already defined
+# Paste your code into this box
+rate = annualInterestRate/12
+mini = 0
+newbalance = balance
+while (newbalance > 0):
+    newbalance = balance
+    mini += 10
+    for i in range(1,13):
+         newbalance = newbalance - mini + (newbalance - mini)*rate
+print  'Lowest Payment: '  + str(mini)

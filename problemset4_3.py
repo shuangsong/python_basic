8a7c88342ec0c57bee5ea88fa5bb40b0c64554d6
shuangsong <shuangsong6@gmail.com>
2016-04-08 16:51:32 -0400

    :clock6:

--- problemset4_3.py
+++ problemset4_3.py
@@ -13,7 +13,15 @@ def isValidWord(word, hand, wordList):
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
+    if word == "" :
+        return False
+    elif word in wordList:
+        return True
+    elif 
 
+print isValidWord("hello", hand, "abcdefghhi")
+                          
+    
 
 
 

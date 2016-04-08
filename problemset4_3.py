eb0e2932c84a667dbc6cd57a714bfa54994332cf
shuangsong <shuangsong6@gmail.com>
2016-04-08 15:24:44 -0400

    :clock5:

---
+++ problemset4_3.py
@@ -0,0 +1,33 @@
+hand = {'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}    
+
+#Valid Words 
+def isValidWord(word, hand, wordList):
+    """
+    Returns True if word is in the wordList and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+
+    Does not mutate hand or wordList.
+   
+    word: string
+    hand: dictionary (string -> int)
+    wordList: list of lowercase strings
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

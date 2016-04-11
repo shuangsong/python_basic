ddbf6464a9552f4077a6036f33a7f6e148fae150
shuangsong <shuangsong6@gmail.com>
2016-04-11 14:46:33 -0400

    :clock3:

---
+++ problemset4_4.py
@@ -0,0 +1,24 @@
+#Hand Length 
+def calculateHandlen(hand):
+    """ 
+    Returns the length (number of letters) in the current hand.
+    
+    hand: dictionary (string int)
+    returns: integer
+    """
+    # TO DO... <-- Remove this comment when you code this function
+    total = 0
+    copy = hand.copy()
+    for k in copy.values():
+        total += k
+        return total
+
+hand = {'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}    
+
+print calculateHandlen(hand)
+
+
+
+
+
+

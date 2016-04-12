54aaee80da09d8f1c678bc293c1676f3bae3ac7d
shuangsong <shuangsong6@gmail.com>
2016-04-12 16:48:27 -0400

    :clock4:

---
+++ problemset6_1.py
@@ -0,0 +1,13 @@
+
+
+
+def buildCoder(shift):
+    """
+    Returns a dict that can apply a Caesar cipher to a letter.
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation, numbers, and spaces.
+
+    shift: 0 <= int < 26
+    returns: dict
+    """
+    ### TODO 
